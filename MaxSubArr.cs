5b9033fe18d441c59662041caf8f8ae25e5fe887
OldProject1 <47574197+OldProject1@users.noreply.github.com>
2020-06-15 11:42:46 +0300

    Update MaxSubArr.cs

--- MaxSubArr.cs
+++ MaxSubArr.cs
@@ -1,55 +1,16 @@
-public int MaxSubArray(int[] nums)
-        {
-            int i = 0;
+public class Solution
+    {
 
+        public int MaxSubArray(int[] nums)
+        {
             int max = nums[0];
-
-            try
+            int maxCurr = max;//max with current element
+            for (int i = 1; i < nums.Length; i++)
             {
-                MoveToNotNegative(ref i, nums, ref max);
-            }
-            catch { return max; }
-
-            max = nums[i];
-
-            int partMax;
-
-            while (true)
-            {
-                try
-                {
-                    MoveToPositive(ref i, nums);
-                }
-                catch { return max; }
-
-                partMax = GetMaxOfPart(ref i, nums);//pointing first element in next part already (it is positive)
-
-                max = Math.Max(max, partMax);
-            }
-        }
-
-        private int GetMaxOfPart(ref int i, int[] nums)
-        {
-            int max = nums[i], 
-                sum = nums[i];
-            i++;
-            while (i <= nums.Length - 1 && sum > 0)
-            {
-                sum += nums[i];
-                max = Math.Max(max, sum);
-                i++;
+                maxCurr = maxCurr < 0 ? nums[i] : maxCurr + nums[i];
+                max = Math.Max(max, maxCurr);
             }
             return max;
         }
 
-        private void MoveToPositive(ref int i, int[] nums)
-        {
-            while (nums[i] <= 0) { i++; }
-        }
-        private void MoveToNotNegative(ref int i, int[] nums, ref int maxElement)
-        {
-            while (nums[i] < 0) {
-                maxElement = Math.Max(maxElement, nums[i]); 
-                i++; 
-            }
-        }
+    }
